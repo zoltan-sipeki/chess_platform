71cb6f77b9ef4f3af6ef9a97a873aa7fb54cddf6
Zoltán Sipeki <sipeki-zoltan@outlook.com>
2026-06-24 11:20:08 +0200

    remove unnecessary code from dashboard component

--- app/src/pages/dashboard/dashboard.component.ts
+++ app/src/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnDestroy, OnInit, signal } from "@angular/core";
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
-import { NgbAlert } from "@ng-bootstrap/ng-bootstrap";
+import { AlertComponent } from "../../components/alert/alert.component";
 import { FriendList } from "../../components/friend-list/friend-list.component";
 import { Navbar } from "../../components/navbar/navbar.component";
 import { NotificationMenu } from "../../components/notification-menu/notification-menu.component";
@@ -8,9 +8,7 @@ import { PlayMenu } from "../../components/play-menu/play-menu.component";
 import { QueueToastComponent } from "../../components/queue-toast/queue-toast.component";
 import { UserMenu } from "../../components/user-menu/user-menu.component";
 import { UserSearch } from "../../components/user-search/user-search.component";
-import { AuthService } from "../../services/AuthService";
 import { BootstrapService } from "../../services/BootstrapService";
-import { AlertComponent } from "../../components/alert/alert.component";
 
 @Component({
     selector: "dashboard",
@@ -20,14 +18,9 @@ import { AlertComponent } from "../../components/alert/alert.component";
 })
 export class Dashboard implements OnInit, OnDestroy {
 
-    private authService: AuthService = inject(AuthService);
-
     private bootstrapService: BootstrapService = inject(BootstrapService);
 
-    logoutUrl = signal<string>("");
-
     ngOnInit(): void {
-        this.logoutUrl.set(this.authService.createLogoutUrl());
         this.bootstrapService.run();
     }
 
