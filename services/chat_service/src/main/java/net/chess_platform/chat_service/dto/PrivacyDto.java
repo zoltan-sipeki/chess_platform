8b5b48b3b8d7ce0a4d3973aaf6f42a71d48e4459
Zoltán Sipeki <sipeki-zoltan@outlook.com>
2026-08-08 21:31:45 +0200

    fix privacy bug

--- services/chat_service/src/main/java/net/chess_platform/chat_service/dto/PrivacyDto.java
+++ services/chat_service/src/main/java/net/chess_platform/chat_service/dto/PrivacyDto.java
@@ -2,6 +2,6 @@ package net.chess_platform.chat_service.dto;
 
 import net.chess_platform.chat_service.model.Privacy;
 
-public record PrivacyDto(Privacy.Restriction.Setting friendsF) {
+public record PrivacyDto(Privacy.Restriction.Setting friends) {
 
 }
